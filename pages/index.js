5687c3a5a3473e5addc57919f733a00f4ce9ba50
Azam <azamkhan561@gmail.com>
2024-01-28 19:46:36 +0530

    added signup functionality

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         {!!user && <Link href="api/auth/logout">Logout</Link>}
         {!user && <>
           <Link href="/api/auth/login" className="rounded-md bg-emerald-500 px-4 py-2 hover:bg-emerald-600">Login</Link>
-          <Link href="/api/auth/login" className="rounded-md bg-emerald-500 px-4 py-2 hover:bg-emerald-600">Signup</Link>
+          <Link href="/api/auth/signup" className="rounded-md bg-emerald-500 px-4 py-2 hover:bg-emerald-600">Signup</Link>
         </>}
       </div>
       </div>
