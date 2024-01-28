a054c1a7f13c14cc406764e20f19b22b28e8f80d
Azam <azamkhan561@gmail.com>
2024-01-28 21:55:17 +0530

    Redirecting to chat page on login

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import Link from "next/link";
-import {useUser} from '@auth0/nextjs-auth0/client';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { getSession } from '@auth0/nextjs-auth0';
 
 export default function Home() {
 
-  const {isLoading, error, user} = useUser();
+  const { isLoading, error, user } = useUser();
 
-  if (isLoading) { return <div>Loading...</div>}
-  if (error) { return <div>{error.message}</div>}
+  if (isLoading) { return <div>Loading...</div> }
+  if (error) { return <div>{error.message}</div> }
   return (
     <>
       <Head>
@@ -15,13 +16,28 @@ export default function Home() {
       </Head>
       <div className="flex min-h-screen w-full bg-gray-800 justify-center items-center text-center text-white">
         <div>
-        {!!user && <Link href="api/auth/logout">Logout</Link>}
-        {!user && <>
-          <Link href="/api/auth/login" className="rounded-md bg-emerald-500 px-4 py-2 hover:bg-emerald-600">Login</Link>
-          <Link href="/api/auth/signup" className="rounded-md bg-emerald-500 px-4 py-2 hover:bg-emerald-600">Signup</Link>
-        </>}
-      </div>
+          {!!user && <Link href="api/auth/logout">Logout</Link>}
+          {!user && <>
+            <Link href="/api/auth/login" className="rounded-md bg-emerald-500 px-4 py-2 hover:bg-emerald-600">Login</Link>
+            <Link href="/api/auth/signup" className="rounded-md bg-emerald-500 px-4 py-2 hover:bg-emerald-600">Signup</Link>
+          </>}
+        </div>
       </div>
     </>
   );
+}
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx.req, ctx.res);
+  if (session) {
+    return {
+      redirect: {
+        destination: "/chat"
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
