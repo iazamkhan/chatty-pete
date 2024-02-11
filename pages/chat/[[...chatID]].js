2e3acd65467050857dd745647f55c4595459b8a1
Azam <azamkhan561@gmail.com>
2024-02-11 12:24:16 +0530

    created and integrated textarea and submit button  for messages in the footer

--- pages/chat/[[...chatID]].js
+++ pages/chat/[[...chatID]].js
@@ -1,7 +1,16 @@
 import Head from "next/head";
-import {ChatSidebar} from 'components/ChatSidebar'
+import { ChatSidebar } from 'components/ChatSidebar'
+import { useState } from "react";
 
-export default function Home() {
+export default function ChatPage() {
+
+  const [messageText, setMessageText] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log("messageText", messageText)
+  }
+
   return (
     <>
       <Head>
@@ -11,7 +20,14 @@ export default function Home() {
         <ChatSidebar />
         <div className="bg-gray-700 flex flex-col">
           <div className="flex-1">chat window</div>
-          <footer className="bg-gray-800 p-10">footer</footer>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+            <fieldset className="flex gap-2">
+              <textarea value={messageText} onChange={e => setMessageText(e.target.value)} placeholder="Send a message..." className="w-full resize-none p-2 rounded-md bg-gray-700 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500" />
+              <button type="submit" className="btn">Send</button>
+            </fieldset>
+            </form>
+          </footer>
         </div>
       </div>
     </>
