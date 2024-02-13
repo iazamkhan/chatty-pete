44b1532c4f5cd6cb4dfbd5f6768bd36a82826e7a
Azam <azamkhan561@gmail.com>
2024-02-13 18:09:16 +0530

    set up openai API call

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -1,3 +1,5 @@
+import { OpenAIEdgeStream } from "openai-edge-stream";
+
 export const config = {
     runtime: "edge"
 }
@@ -5,7 +7,21 @@ export const config = {
 export const handler = async (req) => {
     try {
         const { message } = await req.json();
-    } catch(e) {
+        const stream = await OpenAIEdgeStream(
+            "https://api.openai.com/v1/chat/completions", {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                message: [{content: message, role: "user"}],
+                stream: true
+            })
+        });
+        return new response(stream)
+    } catch (e) {
         console.log("An error occured in sendMessage", e)
     }
 }
