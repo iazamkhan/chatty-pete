b533144bd50c9cc2e63717521bda7c4386d2ae54
Azam <azamkhan561@gmail.com>
2024-02-20 10:43:53 +0530

    updating streamed message in useState and showing on screen

--- pages/chat/[[...chatID]].js
+++ pages/chat/[[...chatID]].js
@@ -6,24 +6,27 @@ import { streamReader } from "openai-edge-stream";
 export default function ChatPage() {
 
   const [messageText, setMessageText] = useState("")
+  const [incomingMessage, setIncomingMessage] = useState("")
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("messageText", messageText)
     const response = await fetch('/api/chat/sendMessage', { 
       method: "POST",
+      stream: true,
       headers: {
         contentType: "application/json"
       },
-      body: JSON.stringify({message: messageText})
+      body: JSON.stringify({message: messageText}),
     });
     const data = response.body;
     if(!data) {
-      return
+      console.log("no data")
     }
     const reader = data.getReader();
     await streamReader(reader, (message) => {
       console.log("Message: ", message);
+      setIncomingMessage(s => `${s}${message.content}`);
     })
   }
 
@@ -35,7 +38,7 @@ export default function ChatPage() {
       <div className="grid h-screen grid-cols-[260px_1fr]">
         <ChatSidebar />
         <div className="bg-gray-700 flex flex-col">
-          <div className="flex-1">chat window</div>
+          <div className="flex-1 text-white">{incomingMessage}</div>
           <footer className="bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
             <fieldset className="flex gap-2">
