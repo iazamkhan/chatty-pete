1a03a0bdb0de5d34f70d06d735b5776457f2f5c6
Azam <azamkhan561@gmail.com>
2024-02-20 16:12:39 +0530

    style changes when the response is being generated

--- pages/chat/[[...chatID]].js
+++ pages/chat/[[...chatID]].js
@@ -10,9 +10,11 @@ export default function ChatPage() {
   const [messageText, setMessageText] = useState("")
   const [incomingMessage, setIncomingMessage] = useState("")
   const [newChatMessages, setNewChatMessages] = useState([])
+  const [generatingResponse, setGeneratingResponse] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setGeneratingResponse(true)
     setNewChatMessages(prev => {
       const newChatMessages = [...prev, {
         _id: uuid(),
@@ -21,7 +23,7 @@ export default function ChatPage() {
       }]
       return newChatMessages;
     })
-    console.log("messageText", messageText)
+    setMessageText("")
     const response = await fetch('/api/chat/sendMessage', {
       method: "POST",
       stream: true,
@@ -36,6 +38,7 @@ export default function ChatPage() {
     }
     const reader = data.getReader();
     setIncomingMessage("This is a dummy reply from ChatGPT");
+    setGeneratingResponse(false)
     await streamReader(reader, (message) => {
       console.log("Message: ", message);
       setIncomingMessage(s => `${s}${message.content}`);
@@ -53,8 +56,8 @@ export default function ChatPage() {
           <div className="flex-1 text-white">{newChatMessages.map(message => <Message key={message._id} role={message.role} content={message.content} />)}{incomingMessage && <Message role="assistant" content={incomingMessage} />}</div>
           <footer className="bg-gray-800 p-10">
             <form onSubmit={handleSubmit}>
-              <fieldset className="flex gap-2">
-                <textarea value={messageText} onChange={e => setMessageText(e.target.value)} placeholder="Send a message..." className="w-full resize-none p-2 rounded-md bg-gray-700 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500" />
+              <fieldset className="flex gap-2" disabled={generatingResponse}>
+                <textarea value={messageText} onChange={e => setMessageText(e.target.value)} placeholder={generatingResponse ? "" : "Send a message..."} className="w-full resize-none p-2 rounded-md bg-gray-700 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500" />
                 <button type="submit" className="btn">Send</button>
               </fieldset>
             </form>
