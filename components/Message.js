c8166234c0c6edbcf8bf798c4bc3e116cfe981bb
Azam <azamkhan561@gmail.com>
2024-02-20 12:51:41 +0530

    showing the avatar with message

--- components/Message.js
+++ components/Message.js
@@ -1,10 +1,32 @@
-function Message({role, content}) {
-  return (
-    <div className="grid grid-cols-[30px_1fr] gap-5 p-5">
-        <div>Avatar</div>
-        <div>{content}</div>
-    </div>
-  )
+import { useUser } from "@auth0/nextjs-auth0/client"
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+
+function Message({ role, content }) {
+
+    const { user } = useUser();
+    console.log("user", user)
+
+    return (
+        <div className={`grid grid-cols-[30px_1fr] gap-5 p-5 ${role === "assistant" ? "bg-gray-600" : ""}`}>
+            <div>{role === "user" && user &&
+                <Image
+                    src={user.picture.src}
+                    width={30}
+                    height={30}
+                    alt="User Avatar"
+                    className="rounded-sm shadow-md shadow-black/50"
+                />
+            }
+            {role === "assistant" &&
+                (<div className="flex h-[30px] w-[30px] items-center justify-center rounded-sm shadow-md shadow-black/50 bg-gray-800">
+                    <FontAwesomeIcon icon={faRobot} className="text-emerald-200" />
+                </div>)}
+                </div>
+            <div>{content}</div>
+        </div>
+    )
 }
 
 export default Message
