d94801402472e73ed6e862e1b6c4303a8518b3f5
Azam <azamkhan561@gmail.com>
2024-02-24 21:26:59 +0530

    formatted response text

--- components/Message.js
+++ components/Message.js
@@ -2,6 +2,7 @@ import { useUser } from "@auth0/nextjs-auth0/client"
 import { faRobot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import ReactMarkdown from "react-markdown";
 
 function Message({ role, content }) {
 
@@ -24,7 +25,7 @@ function Message({ role, content }) {
                     <FontAwesomeIcon icon={faRobot} className="text-emerald-200" />
                 </div>)}
                 </div>
-            <div>{content}</div>
+            <div className="prose prose-invert"><ReactMarkdown>{content}</ReactMarkdown></div>
         </div>
     )
 }
--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -7,6 +7,10 @@ export const config = {
 export default async function handler(req) {
     try {
         const { message } = await req.json();
+        const initialChatMessage = {
+            role: "system",
+            content: "Your name is Chatty Pete. An increadibly intelligent and fast AI Language Processing tool that replies to the queries of people. You were developed by Azam Khan. Your response must be formatted as markdown."
+        }
         const stream = await OpenAIEdgeStream(
             "https://api.openai.com/v1/chat/completions", {
             headers: {
@@ -16,7 +20,7 @@ export default async function handler(req) {
             method: 'POST',
             body: JSON.stringify({
                 model: "gpt-3.5-turbo",
-                message: [{content: message, role: "user"}],
+                message: [initialMessage, {content: message, role: "user"}],
                 stream: true
             })
         });
